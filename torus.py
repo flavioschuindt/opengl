e6c026276651ba1c04c896e9837aefaa53dd096e
Flávio Schuindt <flavio.schuindt@gmail.com>
2014-05-04 15:40:43 -0300

    - Refactoring - Adição de mais um toróide na cena

--- torus.py
+++ torus.py
@@ -3,11 +3,11 @@ from OpenGL.GLU import *
 from OpenGL.GL import *
 from math import pi, cos, sin
  
-torus = None
+scene = []
 
 class Torus:
 
-	def __init__(self, inner_radius=1, outter_radius=0.2, sides=50, rings=50, x_rotated=0, y_rotated=0, z_rotated=0):
+	def __init__(self, color, position, inner_radius=1, outter_radius=0.2, sides=50, rings=50, x_rotated=0, y_rotated=0, z_rotated=0):
 
 		self.inner_radius = inner_radius
 		self.outter_radius = outter_radius
@@ -16,6 +16,8 @@ class Torus:
 		self.x_rotated = x_rotated
 		self.y_rotated = y_rotated
 		self.z_rotated = z_rotated
+		self.color = color
+		self.position = position
 		self.points = []
 
 	def rotate(self, x=0, y=0, z=0):
@@ -23,78 +25,80 @@ class Torus:
 		self.y_rotated += y
 		self.z_rotated += z
 
-	def reshape(self, x, y):
-		if y == 0 or x == 0:
-			return
-		glMatrixMode(GL_PROJECTION)
-		glLoadIdentity()
-		gluPerspective(40.0, x/y, 0.5, 20.0)
-
-		glViewport(0, 0, x, y)
-
-	def _draw(self, numc, numt):
+	def calc_points(self):
 		self.points = []
 		two_pi = pi * 2
 		a = self.outter_radius
 		c = self.inner_radius + self.outter_radius
-		for i in range(numc):
-			glBegin(GL_QUAD_STRIP)
-			for j in range(numt + 1):
+		for i in range(self.sides):
+			for j in range(self.rings + 1):
 				k = 1
 				while k >= 0:
-					s = (i + k) % numc + 0.5
-					t = j % numt
+					s = (i + k) % self.sides + 0.5
+					t = j % self.rings
 
-					x = (c + a * cos(s * two_pi / numc)) * cos(t * two_pi / numt)
-					y = (c + a * cos(s * two_pi / numc)) * sin(t * two_pi / numt) 
-					z = a * sin(s * two_pi / numc)
+					x = (c + a * cos(s * two_pi / self.sides)) * cos(t * two_pi / self.rings)
+					y = (c + a * cos(s * two_pi / self.sides)) * sin(t * two_pi / self.rings) 
+					z = a * sin(s * two_pi / self.sides)
 					k -= 1
 					self.points.append((x, y, z))
-					glVertex3d(x, y, z)
-			glEnd()
 
-	def display(self):
+def display():
+	global scene
+	# clear the drawing buffer.
+	glClear(GL_COLOR_BUFFER_BIT)
+	for torus in scene:
 		glMatrixMode(GL_MODELVIEW)
-		# clear the drawing buffer.
-		glClear(GL_COLOR_BUFFER_BIT)
 		# clear the identity matrix.
 		glLoadIdentity()
 		# traslate the draw by z = -4.0
 		# Note this when you decrease z like -8.0 the drawing will looks far , or smaller.
-		glTranslatef(0.0, 0.0, -4.5)
-		# Red color used to draw.
-		glColor3f(0.8, 0.2, 0.1) 
+		glTranslatef(*torus.position)
+
+		glColor3f(*torus.color) 
 		# changing in transformation matrix.
 		# rotation about X axis
-		glRotatef(self.x_rotated, 1.0, 0.0, 0.0)
+		glRotatef(torus.x_rotated, 1.0, 0.0, 0.0)
 		# rotation about Y axis
-		glRotatef(self.y_rotated, 0.0, 1.0, 0.0)
+		glRotatef(torus.y_rotated, 0.0, 1.0, 0.0)
 		# rotation about Z axis
-		glRotatef(self.z_rotated, 0.0, 0.0, 1.0)
+		glRotatef(torus.z_rotated, 0.0, 0.0, 1.0)
 
-		self._draw(self.sides, self.rings)
+		points = torus.calc_points()
+
+		glBegin(GL_QUAD_STRIP)
+		for point in torus.points:
+			glVertex3d(*point)
+		glEnd()
 
 		#Flush buffers to screen
 		glFlush()
 
-		glutSwapBuffers()
-
-def display():
-	global torus
-	torus.display()
+	glutSwapBuffers()
 
 def reshape(x, y):
-	global torus
-	torus.reshape(x, y)
+	if y == 0 or x == 0:
+		return
+	glMatrixMode(GL_PROJECTION)
+	glLoadIdentity()
+	gluPerspective(40.0, x/y, 0.5, 20.0)
+
+	glViewport(0, 0, x, y)
+	glMatrixMode(GL_MODELVIEW)
+	glutPostRedisplay()
 
 def idle():
-	global torus
-	torus.rotate(y=0.1)
+	for torus in scene:
+		torus.rotate(y=10)
 	display()
 
 def main():
-	global torus
-	torus = Torus(sides=100, rings=100)
+	global scene
+	t1 = Torus(sides=4, rings=4000, color=(0,1,0), position=(1,0,-10))
+	t2 = Torus(sides=4, rings=4000, color=(1,0,0), position=(-1,2,-10))
+
+	scene.append(t1)
+	scene.append(t2)
 
 	glutInit(sys.argv)
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
